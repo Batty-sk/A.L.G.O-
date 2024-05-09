d0f091961dfabe8afac198d6ac26d96f99d4aa85
Batty-sk <saorav.skumar@gmail.com>
2024-05-09 23:52:34 +0530

    added loading screen feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,30 @@ import Header from '@/Components/Header'
 import MainCanvas from '@/Components/MainCanvas'
 import { Provider } from 'react-redux'
 import store from '../store/store'
+import { useEffect,useState } from 'react'
+import RiseLoader from "react-spinners/RiseLoader";
+
+//RiseLoader
+
 import Footer from '@/Components/Footer'
+
 const page = () => {
+  const[loading,setLoading]=useState<boolean>(true)
+
+  useEffect(()=>{
+    setTimeout(() => {
+        setLoading(false)
+    },3000);
+  })
   return (
+    loading? <RiseLoader
+    style={{display:'flex',width:'100%',height:'100vh',justifyContent:'center',alignItems:'center'}}
+    color={'red'}
+    loading={loading}
+    size={150}
+    aria-label="Loading Spinner"
+    data-testid="loader"
+  />:
     <>
     <Provider store={store} >
     <Header/>
