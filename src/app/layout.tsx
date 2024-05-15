4015744decedcff18ac5f5c40cf6f2b467eadb1f
Batty-sk <saorav.skumar@gmail.com>
2024-05-16 00:30:15 +0530

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={roboto.className}>{children}</body>
+      <body className={`${roboto.className} text-black`}>{children}</body>
     </html>
   )
 }
